15a6fb765851b657925c64f74fce13008cafb646
Cari Licor <Carilicor@gmail.com>
2022-10-17 11:55:31 -0400

    first express

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/test',(request, response) => {
+    response.send({ message: "It's all good, man!"})
+})
+
+app.get('/', (request, response) => response.send({ message: 'Welcome to our first API'}))
+
+app.get('/.error', (request, response) => {
+    response.status(500).send({message: 'something went wrong', success: false})
+})
+app.listen(3030,() => console.log("listening on http://localhost:3030..."))
+
